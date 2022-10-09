5466f8fc1d71acff4f9281d3663cb43646a11ce4
Virginie Gaffinet <gaffinet.virginie@gmail.com>
2022-10-09 11:20:56 +0200

    new project added to the portfolio

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,6 +1,17 @@
 
 export const portfolioData = [
   {
+    name: 'La météo de mes villes préférées',
+    informations: "Voici mon projet météo, cette App vous permet de voir la météo de vos villes préférées.",
+    picture: require("../assets/images/ma-meteo.png"),
+    url : 'https://cheery-chebakia-86b669.netlify.app/',
+    githubUrl : 'https://github.com/VirginieGaffinet/meteo_react',
+    html: "HTML",
+    css: "SASS",
+    javascript: "Javascript",
+    React:"React"
+  },
+  {
     name: 'Mes Citations préférées',
     informations: "Voici un projet de mes citations préférées classé par thèmes (les Simpsons, Star wars, Le seigneur des anneaux).",
     picture: require("../assets/images/overview-citations.png"),
@@ -22,24 +33,4 @@ export const portfolioData = [
     javascript: "Javascript",
     React:"React"
   },
-  // {
-  //   name: 'Météo',
-  //   picture: require("../assets/images/react.png"),
-  //   url : 'https://VirginieGaffinet.github.io',
-  //   informations: "Voici un projet de mes citations préférées classé par thèmes (les Simpsons, Star wars, Le seigneur des anneaux).",
-  //   html: "HTML",
-  //   css: "CSS",
-  //   javascript: "Javascript",
-  //   React:"React"
-  // },
-  // {
-  //   name: 'What2Read',
-  //   picture: require("../assets/images/react.png"),
-  //   url : 'https://VirginieGaffinet.github.io',
-  //   informations: "Voici un projet de mes citations préférées classé par thèmes (les Simpsons, Star wars, Le seigneur des anneaux).",
-  //   html: "HTML",
-  //   css: "CSS",
-  //   javascript: "Javascript",
-  //   React:"React"
-  // },
 ];
